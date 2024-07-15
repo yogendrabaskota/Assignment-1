4fe65381b22dd896fff092c2d9d881091b28057d
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-07-15 16:43:34 +0545

    completed

--- wwclass.py
+++ wwclass.py
@@ -3,16 +3,32 @@ class library:
         self.books = {}
         self.issued_books = {}
     
+# for adding books to library
+#    
     def add_books(self, book_name: str):
         if book_name in self.books:
             self.books[book_name] += 1
         else:
             self.books[book_name] = 1
 
+# taking books from library
+
+    def issue_book(self, book_name: str, student):
+        if book_name in self.books and self.books[book_name] > 0:
+            self.books[book_name] -= 1
+            student.borrow_books(book_name)
+            if book_name in self.issued_books:
+                self.issued_books[book_name] +=1
+            else:
+                self.issued_books[book_name] =1
+            return True
+        return False
+    
+
 
 
     def __str__(self):
-        return f'Available books: {self.books} \n'
+        return f'\nAvailable books: {self.books} \n'
     
 
 
@@ -28,22 +44,39 @@ class student:
 
 
     def __str__(self):
-        return f'Student: {self.name}\t Id: {self.std_id} \t borrowed books: {self.borrow_books}'
+        return f'Student: {self.name}\t Id: {self.std_id} \t borrowed books: {self.borrowed_books}\n'
         
 
 
 #Objects:
 lib = library()
 
+#ADDING BOOKS TO LIBRARY
+lib.add_books("physics")
+lib.add_books("physics")
 lib.add_books("physics")
 lib.add_books("chemistry")
-lib.add_books("matheamtics")
+lib.add_books("chemistry")
+lib.add_books("chemistry")
+lib.add_books("mathematics")
+lib.add_books("mathematics")
+lib.add_books("mathematics")
+lib.add_books("mathematics")
 
+
+#ATUDENTS DETAILS
 st1 = student("Ram", 1)
 st2 = student("shyam", 2)
 
 
-# student details of library
+# BOOKS TAKEN BY STUDENTS
+
+lib.issue_book("physics", st1)
+lib.issue_book("chemistry", st2)
+lib.issue_book("physics", st2)
+#lib.take_book("matheamtics", st3)
+
+#SHOWING STUDENT DETAILS 
 
 print(lib)
 print(st1)
